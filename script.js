b648ed0b1f476536021862f1a75d4b60c1ab1a95
wirelesshacker <67308579+wirelesshacker@users.noreply.github.com>
2023-02-02 14:33:08 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const indexLink = document.getElementById("index-link");
+indexLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  alert("You are already on the menu page.");
+});
